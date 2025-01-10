e9fccc9f46c34b90f0e9e748e282ac6057b725db
Ayres Roux <ayresrouxio@gmail.com>
2025-01-10 08:51:29 -0300

    aula: utilizando status code

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import http from "node:http"
 const server = http.createServer((request, response) => {
   const { method } = request
 
-  return response.end("Método:" + method)
+  return response.writeHead(200).end("Método:" + method)
 })
 
 server.listen(3333)
