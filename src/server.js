1d1a7c01d66c502c770682fa34c1e190d7adb819
Ayres Roux <ayresrouxio@gmail.com>
2025-01-10 09:18:41 -0300

    aula: url da requisição

--- src/server.js
+++ src/server.js
@@ -1,9 +1,17 @@
 import http from "node:http"
 
 const server = http.createServer((request, response) => {
-  const { method } = request
+  const { method, url } = request
 
-  return response.writeHead(200).end("Método:" + method)
+  if (method === "GET" && url === "/products") {
+    return response.end("Lista de produtos!")
+  }
+
+  if (method === "POST" && url === "/products") {
+    return response.writeHead(201).end("Produto cadastrado!")
+  }
+
+  return response.writeHead(404).end("Rota não encontrada!")
 })
 
 server.listen(3333)
